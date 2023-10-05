5dce047c95cf3efd72ab541a55b8fafc7973cd62
Lucas Santana Lemos <lslemos59@gmail.com>
2023-10-05 00:07:14 -0300

    navegate

--- meuprojeto/meuapp/views.py
+++ meuprojeto/meuapp/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 rooms = [
-    {'id': 1, 'name': 'Sobre Nos'},
-    {'id': 2, 'name': 'Produtos'},
-    {'id': 3, 'name': 'Ofertas'},
-    {'id': 4, 'name': 'Carrinho'},
-    {'id': 5, 'name': 'Contato'}
+    {'id': 1, 'name': 'sobre nos'},
+    {'id': 2, 'name': 'produtos'},
+    {'id': 3, 'name': 'ofertas'},
+    {'id': 4, 'name': 'carrinho'},
+    {'id': 5, 'name': 'contato'}
 ]
 
 def index(request):
     return render(request, 'html/index.html', {'rooms': rooms})
 
-def produtos (request, pk):
+def produtos(request):
     return render(request, 'html/produtos.html')
+
+def ofertas(request):
+    return render(request, 'html/ofertas.html', {'rooms': rooms})
--- meuprojeto/meuprojeto/urls.py
+++ meuprojeto/meuprojeto/urls.py
@@ -6,5 +6,6 @@ from meuapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
-    path('produtos/<str:pk>', views.produtos, name="produtos")
+    path('produtos/', views.produtos, name="produtos"),
+    path('ofertas/', views.ofertas, name="ofertas")
 ]
