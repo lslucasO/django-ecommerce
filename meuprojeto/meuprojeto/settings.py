56f1487977d5614e0a12a9cd3377256a4e325e5e
Lucas Santana Lemos <lslemos59@gmail.com>
2023-10-07 13:40:47 -0300

    #

--- meuprojeto/meuprojeto/settings.py
+++ meuprojeto/meuprojeto/settings.py
@@ -125,6 +125,8 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img/produtos')
 
+MEDIA_URL = '/img/produtos/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- meuprojeto/meuprojeto/urls.py
+++ meuprojeto/meuprojeto/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from meuapp import views
 
 urlpatterns = [
@@ -11,3 +13,5 @@ urlpatterns = [
     path('carrinho/', views.carrinho, name="carrinho"),
     path('compra/', views.compra, name="compra")
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
