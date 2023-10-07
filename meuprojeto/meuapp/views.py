43131c484be421aeb0d7397d8346da8454d07f20
Lucas Santana Lemos <lslemos59@gmail.com>
2023-10-07 12:13:34 -0300

    #

--- meuprojeto/meuapp/views.py
+++ meuprojeto/meuapp/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
     return render(request, 'html/index.html')
 
 def produtos(request):
-    return render(request, 'html/produtos.html', )
+    produtos = Produto.objects.all()
+    context = {'produtos': produtos}
+    return render(request, 'html/produtos.html', context=context)
 
 def ofertas(request):
     return render(request, 'html/ofertas.html')
